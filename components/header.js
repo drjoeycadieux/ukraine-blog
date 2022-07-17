ff61e7c8f86b9eb37b6f18c0af79b8290849ff97
drjoeycadieux <instructor.joeycadieux@gmail.com>
2022-07-17 18:46:31 -0400

    adding header on top of page.

---
+++ components/header.js
@@ -0,0 +1,12 @@
+// import Link from 'next/link'
+
+
+export default function header() {
+    return (
+        <div className='header-top'>
+            <p>
+                TechTack Technologies LLC united with the people of Ukraine and the international community in condemning these horrific acts of violence against a sovereign nation and its people. We continue to monitor the events in Europe surrounding the unlawful Russian military invasion of Ukraine.
+            </p>
+        </div>
+    )
+}
